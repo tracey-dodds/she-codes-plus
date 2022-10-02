7c2f9cf7903463a2a10dc93f700e113ec988d456
tracey-dodds <114851468+tracey-dodds@users.noreply.github.com>
2022-10-02 15:46:27 +0800

    Update JavaScript.js

--- week5/JavaScript.js
+++ week5/JavaScript.js
@@ -1,12 +1,50 @@
-let apiKey = "708ef12b3e0617a4c8a28e31ae3f4341";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric";
+let now = new Date();
 
-function showTemperature(response) {
-  console.log(response);
+let h3 = document.querySelector("h3");
+
+let minute = now.getMinutes();
+let hours = now.getHours();
+let date = now.getDate();
+
+let year = now.getFullYear();
+
+let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+let day = days[now.getDay()];
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+let month = months[now.getMonth()];
+
+h3.innerHTML = `${day} ${date}th ${month} ${year} at ${hours}:${minute}`;
+
+function showWeather(response) {
   let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = `${temperature}°C`;
+  document.querySelector("#enter-city").innerHTML = response.data.name;
+  document.querySelector("#today-temperature").innerHTML = `${temperature}°C`;
+}
+function searchCity(city) {
+  let apiKey = "708ef12b3e0617a4c8a28e31ae3f4341";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
 }
 
-axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  searchCity(city);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
